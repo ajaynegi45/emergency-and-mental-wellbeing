4b6f423d5688c8a992fdb50f3d2e0bee1b0981c0
ajaynegi45 <ajaynegi3345@icloud.com>
2024-12-02 19:26:09 +0530

    fix: integrated with backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import App from './App.jsx'
 import { Toaster} from 'sonner'
 
@@ -8,6 +7,6 @@ import { Toaster} from 'sonner'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-      <Toaster position="bottom-right"  richColors={true}/>
+    <Toaster position="bottom-right"  richColors={true}/>
   </StrictMode>,
 )
